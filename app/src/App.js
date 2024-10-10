5efd62f18ac7b2f37eead3db541c6d5309058786
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-10-10 11:45:46 +0530

    initial commito

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Portfolio from "./components/About";
+import './index.css';
+function App() {
+  return (
+    <div className="App">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
